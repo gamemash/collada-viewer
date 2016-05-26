f1f26a6b9cd3a6727c93ee70b6c104fd791f14e2
Ronald Hecker <ronald.m.hecker@gmail.com>
2016-05-26 16:34:52 +0200

    enable  antialias: true

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 var ColladaLoader = require('./src/collada_loader.js');
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
